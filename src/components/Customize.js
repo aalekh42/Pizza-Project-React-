37721f4b412e89810a8736b8fdd3d4d1fc2dd3d6
aalekh42 <aalekhpradeep@gmail.com>
2020-12-01 12:50:17 +0530

    Adding Styling for checkbox and mainly onChange event function

--- src/components/Customize.js
+++ src/components/Customize.js
@@ -11,12 +11,12 @@ import Mushroom from '../assets/Mushroom.png';
 
 export default function Customize({ingredients,setIngredients}){
 
-    const changeIngredient=(name)=> {
-        let newIngredients = JSON.parse(JSON.stringify(ingredients));//Creating copy for mutating further since it is immutable.
-        //toggle that ingredient
-        newIngredients[name] =!newIngredients[name]
+    const onChange = (event, name) => {
+        
+        let newIngredients = JSON.parse(JSON.stringify(ingredients));
+        newIngredients[name] = event;
         setIngredients(newIngredients);
-    }
+      };
 return(
     <>
         {JSON.stringify(ingredients)}
@@ -33,8 +33,66 @@ return(
                 </div>
             </div>
             <div className="col" style={{border:"2px solid blue"}}>
-                <input type="checkbox" id="pineapple" name="pineapple" onChange={()=>changeIngredient('pineapple')}/>
-                <label for="pineapple">Pineapple</label>
+                <label class="container-checkbox">
+                  Pineapple
+                    <input 
+                    type="checkbox" 
+                    checked={ingredients["pineapple"]} 
+                    onChange={(event)=>
+                                onChange(event.currentTarget.checked,"pineapple")} 
+                    />
+                    <span className="checkmark"></span>
+                </label>
+                <label class="container-checkbox">
+                  Basil
+                    <input 
+                    type="checkbox" 
+                    checked={ingredients["basil"]} 
+                    onChange={(event)=>
+                                onChange(event.currentTarget.checked,"basil")} 
+                    />
+                    <span className="checkmark"></span>
+                </label>
+                <label class="container-checkbox">
+                  Olive
+                    <input 
+                    type="checkbox" 
+                    checked={ingredients["olive"]} 
+                    onChange={(event)=>
+                                onChange(event.currentTarget.checked,"olive")} 
+                    />
+                    <span className="checkmark"></span>
+                </label>
+                <label class="container-checkbox">
+                  Tomato
+                    <input 
+                    type="checkbox" 
+                    checked={ingredients["tomato"]} 
+                    onChange={(event)=>
+                                onChange(event.currentTarget.checked,"tomato")} 
+                    />
+                    <span className="checkmark"></span>
+                </label>
+                <label class="container-checkbox">
+                  Mushroom
+                    <input 
+                    type="checkbox" 
+                    checked={ingredients["mushoom"]} 
+                    onChange={(event)=>
+                                onChange(event.currentTarget.checked,"mushroom")} 
+                    />
+                    <span className="checkmark"></span>
+                </label>
+                <label class="container-checkbox">
+                  Cheese
+                    <input 
+                    type="checkbox" 
+                    checked={ingredients["cheese"]} 
+                    onChange={(event)=>
+                                onChange(event.currentTarget.checked,"cheese")} 
+                    />
+                    <span className="checkmark"></span>
+                </label>
             </div>
         </div>
     </>      
